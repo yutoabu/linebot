dea6a4acac58e2da1e0af6eff4a07708dda911f7
yutoabu <159253168+yutoabu@users.noreply.github.com>
2026-06-28 12:05:50 +0000

    linebot0

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'line-bot-api', '~> 1.29'
+gem 'dotenv'
+gem 'webrick'
+gem 'rackup', '~> 2.3'
+gem 'puma', '~> 8.0'
---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'sinatra'
+require 'line/bot'
+require 'dotenv'
+Dotenv.load
+
+set :port, 4567
+set :bind, '0.0.0.0'
+# 403エラー回避の魔法
+set :host_authorization, { permitted_hosts: [] }
+set :protection, false
+
+def client
+  @client ||= Line::Bot::Client.new do |config|
+    config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+    config.channel_token  = ENV['LINE_CHANNEL_TOKEN']
+  end
+end
+
+# LINEからのWebhookを受け取るエンドポイント
+post '/callback' do
+  body = request.body.read
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+  events.each do |event|
+    if event.is_a?(Line::Bot::Event::Message) && 
+       event.type == Line::Bot::Event::MessageType::Text
+      message = { type: 'text', text: event.message['text'] }
+      client.reply_message(event['replyToken'], message)
+    end
+  end
+  'OK'
+end
+
+get '/' do
+  'LINE Bot is running!'
+end
